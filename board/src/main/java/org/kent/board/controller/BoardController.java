51f5760539c61e8b350457bcc94a25a05c60e312
mingyu <kentsang45@gmail.com>
2021-04-22 09:48:01 +0900

    21/04/21 : read, modify, delete

--- board/src/main/java/org/kent/board/controller/BoardController.java
+++ board/src/main/java/org/kent/board/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.kent.board.service.BoardService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -49,4 +50,46 @@ public class BoardController {
         return "redirect:/board/list";
 
     }
+
+    @GetMapping({"/read", "/modify"})
+    public void read(@ModelAttribute("requestDTO") PageRequestDTO pageRequestDTO, Long bno, Model model){
+        log.info("============ GET REGISTER ============");
+        model.addAttribute("dto", boardService.get(bno));
+    }
+
+    @PostMapping("/modify")
+    public String postModify(BoardDTO dto,
+                         @ModelAttribute("requestDTO") PageRequestDTO requestDTO,
+                         RedirectAttributes redirectAttributes){
+
+        log.info("============ POST MODIFY ============");
+        log.info("DTO : " + dto);
+
+        boardService.modify(dto);
+
+        redirectAttributes.addAttribute("page",requestDTO.getPage());
+        redirectAttributes.addAttribute("type",requestDTO.getType());
+        redirectAttributes.addAttribute("keyword",requestDTO.getKeyword());
+
+        redirectAttributes.addAttribute("bno",dto.getBno());
+
+        return "redirect:/board/read";
+
+    }
+
+
+    @PostMapping("/remove")
+    public String postRemove(long bno, RedirectAttributes redirectAttributes){
+
+        log.info("============ POST REMOVE ============");
+        log.info("bno: " + bno);
+
+        boardService.removeWithReplies(bno);
+
+        redirectAttributes.addFlashAttribute("msg", bno);
+
+        return "redirect:/board/list";
+
+    }
+
 }
